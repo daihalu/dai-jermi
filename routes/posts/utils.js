f4f99bb0351e56d373f278606cbe4d897cd05771
Nguyễn Việt Đức <vietduc01100001@gmail.com>
2018-09-29 08:25:21 +0700

    Add q request param for searching in posts API

--- routes/posts/utils.js
+++ routes/posts/utils.js
@@ -12,7 +12,7 @@ exports.removeFalsey = (obj) => {
 };
 
 exports.parseConditions = (query) => {
-    const { author, tag, year, month, date } = query;
+    const { author, tag, year, month, date, q } = query;
     const conditions = {};
 
     if (author) conditions.author = author;
@@ -33,6 +33,8 @@ exports.parseConditions = (query) => {
     }
 
     if (lowerBound && upperBound) conditions._createdAt = { $gte: lowerBound, $lt: upperBound };
+    if (q) conditions.title = { $regex: q, $options: 'gi' };
+
     conditions._approved = true;
 
     return conditions;
