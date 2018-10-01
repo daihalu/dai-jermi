763cb571816d6e761447c6d91ec0707713919695
Nguyễn Việt Đức <vietduc01100001@gmail.com>
2018-10-01 08:18:41 +0700

    Set CORS headers

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const express = require('express');
 const MongoDB = require('./databases/mongodb');
+const cors = require('cors');
 const routes = require('./routes');
 const runTasks = require('./tasks');
 const { PORT } = require('./config');
@@ -10,6 +11,7 @@ const server = http.createServer(app);
 
 MongoDB.init();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.disable('x-powered-by');
